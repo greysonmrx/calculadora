6b8586477bc4c97db0714ef4d4d82988e5ce1dfc
Greyson <greysonmrx@gmail.com>
2019-09-10 09:19:08 -0300

    Contribuição de Greyson Mascarenhas Santos Filho, turma 514

--- calculadora.py
+++ calculadora.py
@@ -1,27 +1,36 @@
-#Complete as funcoes a seguir
+# Complete as funcoes a seguir
 
 def soma(a, b):
-	#Insira o codigo aqui
+	# Código para realizar uma soma
+	print("\nA soma de {x} + {y} é: {result}".format(x = a, y = b, result = a + b))
 
 def subtrai(a, b):
-	#Insira o codigo aqui
+	# Código para realizar uma subtração
+	print("\nA subtração de {x} - {y} é: {result}".format(x = a, y = b, result = a - b))
 
 def multiplica(a, b):
-	#Insira o codigo aqui
+	# Código para realizar uma multiplicação
+	print("\nA multiplicação de {x} x {y} é: {result}".format(x = a, y = b, result = a * b))
 
 def divide(a, b):
-	#Insira o codigo aqui
+	# Verificação simples para a calculadora não dividir por zero
+	if (b == 0):
+		print("\nNão é possível dividir por zero!")
+	else:
+		# Código para realizar uma divisão
+		print("\nA divisão de {x} ÷ {y} é: {result}".format(x = a, y = b, result = a / b))
 
 
-#Programa principal
+# Programa principal
 
 print("Calculadora simples")
 
+# Recebendo dois números do usuário
 num1 = float(input("Insira o primeiro numero: "))
 num2 = float(input("Insira o segundo numero: "))
 
+# Chamando as funções criadas anteriormente
 soma(num1, num2)
 subtrai(num1, num2)
 multiplica(num1, num2)
 divide(num1, num2)
-
